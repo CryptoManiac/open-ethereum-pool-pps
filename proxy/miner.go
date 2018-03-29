6a928ce929f0574c388472209caf874253bd82a8
CryptoManiac <balthazar@yandex.ru>
2018-03-29 03:53:24 +0300

    Return actual share difficulty (Very experimental)

--- proxy/miner.go
+++ proxy/miner.go
@@ -42,11 +42,15 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 		mixDigest:   common.HexToHash(mixDigest),
 	}
 
-	if !hasher.Verify(share) {
+	isShare, actualDiff := hasher.Verify(share)
+
+	if !isShare {
 		return false, false
 	}
 
-	if hasher.Verify(block) {
+	isBlock, _ := hasher.Verify(block)
+
+	if isBlock {
 		ok, err := s.rpc().SubmitBlock(params)
 		if err != nil {
 			log.Printf("Block submission failure at height %v for %v: %v", h.height, t.Header, err)
@@ -55,7 +59,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			return false, false
 		} else {
 			s.fetchBlockTemplate()
-			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, shareFee, h.diff.Int64(), h.height, s.hashrateExpiration)
+			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, actualDiff, shareFee, h.diff.Int64(), h.height, s.hashrateExpiration)
 			if exist {
 				return true, false
 			}
@@ -67,7 +71,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
 		}
 	} else {
-		exist, err := s.backend.WriteShare(login, id, params, shareDiff, shareFee, h.diff.Int64(), h.height, s.hashrateExpiration)
+		exist, err := s.backend.WriteShare(login, id, params, shareDiff, actualDiff, shareFee, h.diff.Int64(), h.height, s.hashrateExpiration)
 		if exist {
 			return true, false
 		}
--- storage/redis.go
+++ storage/redis.go
@@ -166,7 +166,7 @@ func (r *RedisClient) checkPoWExist(height uint64, params []string) (bool, error
 	return val == 0, err
 }
 
-func (r *RedisClient) WriteShare(login, id string, params []string, diff int64, fee float64, netDiff int64, height uint64, window time.Duration) (bool, error) {
+func (r *RedisClient) WriteShare(login, id string, params []string, diff int64, actualDiff int64, fee float64, netDiff int64, height uint64, window time.Duration) (bool, error) {
 	exist, err := r.checkPoWExist(height, params)
 	if err != nil {
 		return false, err
@@ -182,14 +182,14 @@ func (r *RedisClient) WriteShare(login, id string, params []string, diff int64, 
 	ts := ms / 1000
 
 	_, err = tx.Exec(func() error {
-		r.writeShare(tx, ms, ts, login, id, diff, fee, netDiff, window)
+		r.writeShare(tx, ms, ts, login, id, diff, actualDiff, fee, netDiff, window)
 		tx.HIncrBy(r.formatKey("stats"), "roundShares", diff)
 		return nil
 	})
 	return false, err
 }
 
-func (r *RedisClient) WriteBlock(login, id string, params []string, diff int64, fee float64, roundDiff int64, height uint64, window time.Duration) (bool, error) {
+func (r *RedisClient) WriteBlock(login, id string, params []string, diff int64, actualDiff int64, fee float64, roundDiff int64, height uint64, window time.Duration) (bool, error) {
 	exist, err := r.checkPoWExist(height, params)
 	if err != nil {
 		return false, err
@@ -205,7 +205,7 @@ func (r *RedisClient) WriteBlock(login, id string, params []string, diff int64, 
 	ts := ms / 1000
 
 	cmds, err := tx.Exec(func() error {
-		r.writeShare(tx, ms, ts, login, id, diff, fee, roundDiff, window)
+		r.writeShare(tx, ms, ts, login, id, diff, actualDiff, fee, roundDiff, window)
 		tx.HSet(r.formatKey("stats"), "lastBlockFound", strconv.FormatInt(ts, 10))
 		tx.HDel(r.formatKey("stats"), "roundShares")
 		tx.ZIncrBy(r.formatKey("finders"), 1, login)
@@ -230,7 +230,7 @@ func (r *RedisClient) WriteBlock(login, id string, params []string, diff int64, 
 	}
 }
 
-func (r *RedisClient) writeShare(tx *redis.Multi, ms, ts int64, login, id string, diff int64, fee float64, netDiff int64, expire time.Duration) {
+func (r *RedisClient) writeShare(tx *redis.Multi, ms, ts int64, login, id string, diff int64, actualDiff int64, fee float64, netDiff int64, expire time.Duration) {
 	tx.HIncrByFloat(r.formatKey("miners", login), "balance", util.GetShareReward(diff, netDiff, fee))
 	tx.HIncrBy(r.formatKey("shares", "roundCurrent"), login, diff)
 	tx.ZAdd(r.formatKey("hashrate"), redis.Z{Score: float64(ts), Member: join(diff, login, id, ms)})
