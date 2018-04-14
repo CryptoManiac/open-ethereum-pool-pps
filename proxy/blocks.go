8bdb1b5dd5c728b0c2ac113229cc013e7f2ee7cc
CryptoManiac <balthazar.ad@gmail.com>
2018-04-14 21:06:37 +0300

    If condition is unnecessary here

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -91,14 +91,12 @@ func (s *ProxyServer) fetchBlockTemplate() {
 	log.Printf("New block to mine on %s at height %d / %s", rpc.Name, height, reply[0][0:10])
 
 	// Stratum Proxy jobs
-	if s.config.Proxy.Stratum.Enabled {
-		switch s.config.Proxy.Stratum.Protocol {
-		case "Stratum-Proxy":
-			go s.broadcastNewSPJobs()
-		case "EthereumStratum":
-			go s.broadcastNewESJobs()
-		default:
-		}
+	switch s.config.Proxy.Stratum.Protocol {
+	case "Stratum-Proxy":
+		go s.broadcastNewSPJobs()
+	case "EthereumStratum":
+		go s.broadcastNewESJobs()
+	default:
 	}
 }
 
